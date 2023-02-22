13726b89d0c2edee521005e69653eba2b4c4796c
jinhuaiwang <2965836218@qq.com>
2023-02-22 14:24:51 +0800

    Optimization code

--- build-image.sh
+++ build-image.sh
@@ -9,35 +9,35 @@ Usage: $(basename "${BASH_SOURCE[0]}") [h] [1] [2] [3] [4] [q]
 💁 This script will create fully-automated Ubuntu release version 20 to 22 installation media.
 
 Available options:
-       h Get the current menu
-       1 Build the base image
-       2 Build image with installer
-       3 Build image with installer and local services, esxi only
-       4 Build image with installer and local services, only for kvm
+       p Get the current menu
+       s Build the base image
+       n Build image with installer
+       a Build image with installer and local services, esxi only
+       k Build image with installer and local services, only for kvm
        q Exit script
-exit
 EOF
 }
+menu
 
-
-read -p "please input your build images ID:" value
+read -p "please input your build images ID: " value
 case $value in
-    h)
+    p)
     menu
+    exit
     ;;
-    1)
+    s)
     bash -x  ubuntu-autoinstall-generator-tools.sh  -a  -u example/user-data-simple.yml -n focal -d ubuntu-autoinstall-simple.iso
     ;;
-    2)
+    n)
     bash -x  ubuntu-autoinstall-generator-tools.sh  -a  -u example/user-data-kvm-esxi.yml -n focal -p package-name.txt  -d ubuntu-autoinstall-pkgs.iso
     ;;
-    3)
+    a)
     bash -x  ubuntu-autoinstall-generator-tools.sh  -a  -u  example/user-data-local-service.yml -n focal -p package-name.txt -c example/config-all.sh  \
               -j rc.local -s /root/service -d ubuntu-autoinstall-local-service.iso
     ;;
-    4)
+    k)
     bash -x  ubuntu-autoinstall-generator-tools.sh  -a  -u example/user-data-kvm-esxi.yml -n focal -p package-name.txt -c example/config-kvm.sh \
-          -j rc-kvm.local -t template/silver-peak-hardware-template.xml  -s /root/service -d ubuntu-autoinstall-silver-peak.iso -k 0
+          -j rc-kvm.local -t template/silver-peak-template.xml  -s /root/service -d ubuntu-autoinstall-silver-peak.iso -k 0
     ;;
     q)
     exit
--- example/config-kvm.sh
+++ example/config-kvm.sh
@@ -2,7 +2,6 @@
 #
 
 # setup root login
-sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
 
 # change root password
@@ -11,8 +10,7 @@ echo root:test123 | chpasswd
 # get all nic name for operation system, configure the one of Nic name
 function get_first_nic_name() {
 #!/bin/bash
-nic_names=$(cat /proc/net/dev | awk '{i++; if(i>2){print $1}}' | sed 's/^[\t]*//g' | sed 's/[:]*$//g' | grep -v "lo" | grep -v "macvtap")
-
+nic_names=$(ls /sys/class/net/ | grep -v "`ls /sys/devices/virtual/net/`")
 new_nic_names=()
 short_nic=${nic_names[0]}
 for i in ${nic_names[*]};do
@@ -32,8 +30,7 @@ for i in ${nic_names[*]};do
 done
 
 nic_sort_names=$(echo ${new_nic_names[*]} | tr ' ' '\n' | sort -n)
-echo ${nic_sort_names} | tr -s "\r\n" " "
- > /dev/null
+echo ${nic_sort_names} | tr -s "\r\n" " " > /dev/null
 nic_name=$(echo ${nic_sort_names} | awk '{print $1}')
 echo $nic_name
 sed -i "s/    ens136/    ${nic_name}/g" /etc/netplan/00-installer-config.yaml
@@ -54,25 +51,19 @@ EOF
 # Enable serial console on Ubuntu
 # GRUB_SERIAL_COMMAND="serial --speed=9600" or GRUB_SERIAL_COMMAND="serial --speed=9600 --unit=0 --word=8 --parity=no --stop=1"
 sed -i 's/^#\?GRUB_TERMINAL=.*/GRUB_TERMINAL=serial/g' /etc/default/grub
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/g' /etc/default/grub
+#sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/g' /etc/default/grub
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT=""/g' /etc/default/grub
 sed -i 's/GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX="console=tty0 console=ttyS0,9600"/g' /etc/default/grub
 grep "GRUB_SERIAL_COMMAND" /etc/default/grub > /dev/null 2>&1 || sed -i '/GRUB_CMDLINE_LINUX=.*/a\GRUB_SERIAL_COMMAND="serial --speed=9600"' /etc/default/grub
 #update grup
 update-grub
 
+sed -i 's/^#\?DefaultTimeoutStopSec=.*/DefaultTimeoutStopSec=3s/g' /etc/systemd/system.conf
+sed -i 's/^#\?DefaultTimeoutStartSec=.*/DefaultTimeoutStartSec=3s/g' /etc/systemd/system.conf
+systemctl daemon-reload
+
 # Location of template file
 template_file="/opt/template.xml"
-echo "Generate mac address and cpu model"
-generate_mac_num=$(grep "mac_str"  ${template_file} | wc -l)
-for((i=1;i<=${generate_mac_num};i++)); do
-        # Generate mac address
-        mac=$(echo $RANDOM|md5sum|sed 's/../:&/g'|cut -c 1-15)
-        constant="00"
-        mac_str=${constant}${mac}
-        mac_str_constant=$(echo mac_str${i})
-        # to change the mac address from template file
-        sed -i "s/${mac_str_constant}/${mac_str}/g" ${template_file}
-done
 
 # add cpu number for template file
 cpu_number=$(cat /proc/cpuinfo |grep "physical id"|sort |uniq|wc -l)
@@ -84,15 +75,23 @@ free_size=$((${mem_total}-638812))
 sed -i "s/free_mem/${free_size}/g" ${template_file}
 
 # add network interface card for template file
-nic_list=$(cat /proc/net/dev | awk '{i++; if(i>2){print $1}}' | sed 's/^[\t]*//g' | sed 's/[:]*$//g' | grep -v "lo" | sort -n | grep -v "macvtap")
-j=0
+#nic_list=$(cat /proc/net/dev | awk '{i++; if(i>2){print $1}}' | sed 's/^[\t]*//g' | sed 's/[:]*$//g' | grep -v "lo" | sort -n | grep -v "macvtap")
+
+# get all of the physical nic names
+nic_list=$(ls /sys/class/net/ | grep -v "`ls /sys/devices/virtual/net/`")
+j=-1
 # shellcheck disable=SC2068
 for i in ${nic_list[@]}; do
   j=$(expr $j + 1)
   nic_str="nic_str"
   nic_str_constant=$(echo nic_str${j})
 	# to change the mac address from template file
-  sed -i "s/${nic_str_constant}/${i}/g" ${template_file}
+	grep ${nic_str_constant}  ${template_file}
+	if [ $? -eq 0 ];then
+	    sed -i "s/${nic_str_constant}/${i}/g" ${template_file}
+	else
+	    break
+	fi
 done
 
 # centos 7 is ture
